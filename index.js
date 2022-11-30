8f0ddd8f6c702d8d802269870339afef4f16ac14
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-11-30 15:44:26 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const chai = "chai"
+const spy = "chai-spies"
+
+function receivesAFunction(spy){
+    spy();
+}
+function fun(activities) {
+    return "Playing at the beach"
+    
+}
+function returnsANamedFunction(activities) {
+    return fun
+}
+
+function returnsAnAnonymousFunction() {
+   return fun
+}
